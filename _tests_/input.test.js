728a3f013be6d950304d1a9159d23efc418325ed
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-15 10:43:13 -0800

    add a second passing test

--- _tests_/input.test.js
+++ _tests_/input.test.js
@@ -7,4 +7,12 @@ describe('Input', () => {
     expect(newVar.inputAge).toEqual("26");
     expect(newVar.inputExpectancy).toEqual("100");
   });
+
+  test('should convert the variable to a number', () => {
+    let newNum = new Input("a","100");
+    expect(newNum.convertToNum()).toEqual("enter a number.");
+    // expect(newNum.inputExpectancy).toEqual(100);
+    // expect(newNum.convertToNum()).toEqual("You are 26 years old and should live to be 100 years old.");
+  })
+
 });
--- src/age-calc.js
+++ src/age-calc.js
@@ -2,6 +2,15 @@ export class Input {
   constructor(inputAge,inputExpectancy) {
     this.inputAge = inputAge;
     this.inputExpectancy = inputExpectancy;
+    this.age = 0;
+    this.expectancy = 0;
   }
-
+  convertToNum() {
+    if ((isNaN(this.inputAge)) || (isNaN(this.inputExpectancy)))  {
+      return "enter a number."
+    }
+    // let age = parseInt(this.inputAge);
+    // let expectancy = parseInt(this.inputExpectancy);
+    // expect(newNum.convertToNum()).toEqual("You are 26 years old and should live to be 100 years old.");
+  }
 }
